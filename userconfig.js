2d32ecf1f44b9e9b6bd972df20574e826925f899
Vincent BESSON <120799125+vibr77@users.noreply.github.com>
2025-09-17 11:25:15 +0200

    v4

--- userconfig.js
+++ userconfig.js
@@ -54,13 +54,7 @@ const default_configuration = {
               url: "https://app.raindrop.io",
               icon: "droplet-bolt",
               icon_color: palette.green,
-            },
-            {
-              name: "musicForProgramming();",
-              url: "https://musicforprogramming.net",
-              icon: "binary-tree",
-              icon_color: palette.peach,
-            },
+            }
           ],
         },
         {
@@ -108,6 +102,12 @@ const default_configuration = {
               icon: "brand-google-drive",
               icon_color: palette.blue,
             },
+             {
+              name: "nocodb",
+              url: "https://nocodb.b3s.net",
+              icon: "brand-google-drive",
+              icon_color: palette.blue,
+            },
           ],
         },
         {
@@ -119,30 +119,7 @@ const default_configuration = {
               icon: "news",
               icon_color: palette.green,
             },
-            {
-              name: "mil.in.ua",
-              url: "https://mil.in.ua",
-              icon: "badge-filled",
-              icon_color: palette.peach,
-            },
-            {
-              name: "куток",
-              url: "https://kutok.io",
-              icon: "border-radius",
-              icon_color: palette.red,
-            },
-            {
-              name: "ґрунт",
-              url: "https://grnt.media",
-              icon: "eye-bolt",
-              icon_color: palette.blue,
-            },
-            {
-              name: "village",
-              url: "https://www.village.com.ua",
-              icon: "home-2",
-              icon_color: palette.mauve,
-            },
+            
           ],
         },
       ],
